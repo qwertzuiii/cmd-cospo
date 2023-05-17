2385f5122eba7b6cb418e9f1b1bbc71f812be94e
qwertzuiii <78412852+qwertzuiii@users.noreply.github.com>
2023-05-17 13:52:55 +0200

    Added config & Renamed `bin/` folder to `ccmd/`
    - Added Font changing in `config.toml`

--- cospo.py
+++ cospo.py
@@ -1,30 +1,25 @@
 import os
-import importlib
-import bin.plugins.gpkg_reader as gpkg
+import runpy
+import ccmd.plugins.gpkg_reader as gpkg
 import json
+import tomllib
 
-if os.path.exists('bin/project.gpkg'):
-    if os.path.exists('bin/version.gpkg'):
-        itsRootIThink = True
-    else:
-        itsRootIThink = False
-else:
-    itsRootIThink = False
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-if itsRootIThink:
-    gpkg_project = open('bin/project.gpkg', 'rb').read()
-    gpkg_project = gpkg.read(gpkg_project)
-    gpkg_project = json.loads(gpkg_project)
-    gpkg_version = open('bin/version.gpkg', 'rb').read()
-    gpkg_version = gpkg.read(gpkg_version)
-    gpkg_version = json.loads(gpkg_version)
+gpkg_project = open(SCRIPT_PATH + '/ccmd/project.gpkg', 'rb').read()
+gpkg_project = gpkg.read(gpkg_project)
+gpkg_project = json.loads(gpkg_project)
+gpkg_version = open(SCRIPT_PATH + '/ccmd/version.gpkg', 'rb').read()
+gpkg_version = gpkg.read(gpkg_version)
+gpkg_version = json.loads(gpkg_version)
 
+tomlfile = open(SCRIPT_PATH + "/ccmd/config.toml", 'r').read()
+toml_cfg = tomllib.loads(tomlfile)
+toml_c = toml_cfg['console']
+toml_f = toml_cfg['font']
 
 def startHeader():
-    if itsRootIThink:
-        print(' {}, by {} - version {} ({})'.format(gpkg_project['project_name'].upper(), gpkg_project['project_creator'], gpkg_version['version'], gpkg_version['buildnum']))
-    else:
-        print(' CMD-COSPO, by qwertzuiii - version / (/)')
+    print(' {}, by {} - version {} ({})'.format(gpkg_project['project_name'].upper(), gpkg_project['project_creator'], gpkg_version['version'], gpkg_version['buildnum']))
     print()
     print()
 
@@ -34,6 +29,9 @@ class errors:
     
     def module_not_found(module_name=""):
         print(f'Module "{module_name}" not found/installed!')
+    
+    def module_importing_exception(e=""):
+        print(f'Error while importing module: {str(e)}')
 
     def attribute_error(module_name=""):
         print(f'run() cannot be found in module "{module_name}"!')
@@ -43,24 +41,37 @@ class cmdlist:
         print('HEKL')
         pass
 
-    def runcode(ARGV):
+    def runcode(ARGV, func_name="run", func_arguments = None):
         if len(ARGV) < 2:
             return errors.not_enough_arguments()
 
-        if os.path.exists('bin/plugins/'+ARGV[1]+'.py'):
-            x = 'bin.plugins.'+ARGV[1]
+        if os.path.exists(SCRIPT_PATH + '\\ccmd\\plugins\\' + ARGV[1] + '.py'):
+            x = SCRIPT_PATH + '\\ccmd\\plugins\\' + ARGV[1] + '.py'
         else:
             x = ARGV[1]
+        
+        #print(x)
 
-        print(x, ARGV[1])
+        #print(x, ARGV[1])
         
+        #  OLD IMPORTING METHOD
+        # try:
+        #     lib = importlib.import_module(x)
+        # except ModuleNotFoundError:
+        #     return errors.module_not_found(ARGV[1])
+
         try:
-            lib = importlib.import_module(x)
-        except ModuleNotFoundError:
+            plugin = runpy.run_path(x)
+        except FileNotFoundError:
             return errors.module_not_found(ARGV[1])
+        except Exception as e:
+            return errors.module_importing_exception(e)
         
         try:
-            lib.run()
+            if func_arguments == None:
+                plugin[func_name]()
+            else:
+                plugin[func_name](func_arguments)
         except AttributeError:
             return errors.attribute_error(ARGV[1])
 
@@ -71,9 +82,17 @@ commands = {
 }
 
 cwd = os.getcwd()
-prompt_prefix = 'cospo @ {} $ '.format(cwd)
 
-startHeader()
+if toml_c['prompt_prefix'] == "DEFAULT":
+    prompt_prefix = 'cospo @ {} $ '.format(cwd)
+else:
+    prompt_prefix = toml_c['prompt_prefix']
+
+if toml_f["change_font"]:
+    cmdlist.runcode(['', 'console_fontchanging'], func_name="_FONT_CHANGE", func_arguments=toml_f["font_name"])
+
+if toml_c["start_with_header"]:
+    startHeader()
 
 while True:
 
